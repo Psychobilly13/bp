1211d991fedff10380c31ce4155532ea3c6e1707
Psychobilly13 <psuchomort18@gmail.com>
2023-10-12 19:09:44 +0200

    feat: added cli

--- markoPolo/src/index.js
+++ markoPolo/src/index.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 async function app() {
     /** init */
     const app = express()
-    const port = process.env.PORT;
-    const host = process.env.HOST
+    const port = process.env.PORT || 3000;
+    const host = process.env.HOST || 'localhost'
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
