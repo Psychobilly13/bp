40ba740a5782ed507e55b09cee83da3f08e0798e
Psychobilly13 <psuchomort18@gmail.com>
2023-10-13 10:56:12 +0200

    feat: added http api

--- markoPolo/src/routes/game.route.js
+++ markoPolo/src/routes/game.route.js
@@ -1,69 +1,41 @@
-const { createRangeArray } = require("./utils/array");
+const { validateNumberParam, validateArrayOfNumberBody, validateRange } = require('../middlewares/validation')
+const { createRangeArray } = require('../utils/array')
 
-async function gameRoute(app) {
-    app.get('/game/:number', async (req, res) => {
-        try {
-            const number = parseInt(req.params.number);
+async function gameRoute (app) {
+  app.get('/game/:number', validateNumberParam, async (req, res) => {
+    try {
+      const number = req.validatedNumber
+      const result = await req.gameService.startMarkoPolo(number)
+      res.json(result)
+    } catch (err) {
+      res.status(err.code || 500).json({ error: `${err.message}` })
+    }
+  })
 
-            /** validation */
-            if (typeof number !== 'number' || isNaN(number)) {
-                const err = new Error("get.game.param.shouldBeNumber")
-                err.code = 400;
-                throw err
-            }
+  app.post('/game/list', validateArrayOfNumberBody, async (req, res) => {
+    try {
+      const numbers = req.validatedArrayOfNumber
+      const results = await Promise.all(numbers.map(number => req.gameService.startMarkoPolo(number)))
+      res.json(results)
+    } catch (err) {
+      res.status(err.code || 500).json({ error: `${err.message}` })
+    }
+  })
 
-            const result = await req.gameService.startMarkoPolo(number);
-            res.json(result);
-        } catch (err) {
-            res.status(err.code || 500).json({ error: `${err.message}` });
-        }
-    });
+  app.post('/game/range', validateRange, async (req, res) => {
+    try {
+      const range = req.validatedRange
 
-    app.post('/game/list', async (req, res) => {
-        try {
-            const numbers = req.body;
+      const [start, end] = range
 
-            /** validation */
-            if (!Array.isArray(numbers) || numbers.some(item => typeof item !== 'number')) {
-                const err = new Error("post.game.body.shouldBeArrayOfNumbers");
-                err.code = 400;
-                throw err;
-            }
+      const numbers = await createRangeArray(start, end)
 
-            const results = await Promise.all(numbers.map(number => req.gameService.startMarkoPolo(number)));
-            res.json(results);
-        } catch (err) {
-            res.status(err.code || 500).json({ error: `${err.message}` });
-        }
-    });
-
-    app.post('/game/range', async (req, res) => {
-        try {
-            const range = req.body;
-
-            /** validation */
-            if (!Array.isArray(range) || range.some(item => typeof item !== 'number')) {
-                const err = new Error("post.game.body.shouldBeArrayOfNumbers");
-                err.code = 400;
-                throw err;
-            }
-            if (range.length !== 2) {
-                const err = new Error("post.game.body.shouldBeFromTwoNumbers");
-                err.code = 400;
-                throw err;
-            }
-
-            const [start, end] = req.body;
-
-            const numbers = await createRangeArray(start, end);
-
-            const results = await Promise.all(numbers.map(number => req.gameService.startMarkoPolo(number)));
-            res.json(results);
-        } catch (err) {
-            res.status(err.code || 500).json({ error: `${err.message}` });
-        }
-    });
+      const results = await Promise.all(numbers.map(number => req.gameService.startMarkoPolo(number)))
+      res.json(results)
+    } catch (err) {
+      res.status(err.code || 500).json({ error: `${err.message}` })
+    }
+  })
 }
 
-
-module.exports = gameRoute;
+module.exports = gameRoute
--- markoPolo/src/services/game.service.js
+++ markoPolo/src/services/game.service.js
@@ -1,12 +1,12 @@
 class GameService {
-    async startMarkoPolo(num) {
-        /** check if the number is divisible by 3 and 5 */
-        const isDivisibleBy3 = num % 3 === 0;
-        const isDivisibleBy5 = num % 5 === 0;
+  async startMarkoPolo (num) {
+    /** check if the number is divisible by 3 and 5 */
+    const isDivisibleBy3 = num % 3 === 0
+    const isDivisibleBy5 = num % 5 === 0
 
-        /** use a ternary operator to determine the output based on divisibility */
-        return isDivisibleBy3 && isDivisibleBy5 ? "Марко Полло" : isDivisibleBy3 ? "Марко" : isDivisibleBy5 ? "Полло" : num;
-    }
+    /** use a ternary operator to determine the output based on divisibility */
+    return isDivisibleBy3 && isDivisibleBy5 ? 'Марко Полло' : isDivisibleBy3 ? 'Марко' : isDivisibleBy5 ? 'Полло' : num
+  }
 }
 
-module.exports = GameService;
+module.exports = GameService
