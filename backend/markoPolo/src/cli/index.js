42ca871325ccddfb009d91a93e96064593c4d737
Psychobilly13 <psuchomort18@gmail.com>
2023-10-14 12:59:16 +0200

    fix: lint

--- backend/markoPolo/src/cli/index.js
+++ backend/markoPolo/src/cli/index.js
@@ -4,13 +4,14 @@ const { program } = require('commander')
 const { getRandomNumberInRange } = require('../utils/number')
 
 program
-  .command('markoPolo:randomInRangeFrom1to100')
-  .description('Play in Marko Polo with random in range from 1 to 100')
+  .command('markoPolo:randomInRangeFrom1to1000')
+  .description('Play in Marko Polo with random in range from 1 to 1000')
   .action(async () => {
-    const number = getRandomNumberInRange(1, 100)
+    const number = getRandomNumberInRange(1, 1000)
     const gameService = new GameService()
     const result = await gameService.startMarkoPolo(number)
-    console.log(result)
+    console.log("number:", number)
+    console.log("result:", result)
   })
 
 program.parse(process.argv)
