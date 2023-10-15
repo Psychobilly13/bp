61efb9e0799932f4e85bb4334f26ef873bf3e27b
Psychobilly13 <psuchomort18@gmail.com>
2023-10-15 11:44:57 +0200

    hotfix: error

--- backend/betParser/src/externalApi/olimpBet/index.js
+++ backend/betParser/src/externalApi/olimpBet/index.js
@@ -3,13 +3,16 @@ require('dotenv').config()
 const url = process.env.OLIMP_BET_URL
 
 async function startOlimpBet () {
+  try {
   const response = await fetch(url, {
     headers: {
       'Accept-Language': 'ru-RU'
     }
   })
   const html = await response.json()
-  return html
+  return html } catch (err) {
+    throw new Error(`${err}`)
+  }
 }
 
 module.exports = { startOlimpBet }
--- backend/betParser/src/externalApi/olimpCom/index.js
+++ backend/betParser/src/externalApi/olimpCom/index.js
@@ -3,13 +3,16 @@ require('dotenv').config()
 const url = process.env.OLIMP_COM_URL
 
 async function startOlimpCom () {
+  try {
   const response = await fetch(url, {
     headers: {
       'Accept-Language': 'ru-RU'
     }
   })
   const html = await response.text()
-  return html
+  return html } catch (err) {
+    throw new Error(`${err}`)
+  }
 }
 
 module.exports = { startOlimpCom }
